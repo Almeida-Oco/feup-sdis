98f625b8455a540c33861e97a17c67f0d741b8b9
Almeida-Oco <intjalmeida@gmail.com>
2018-02-21 11:35:04 +0000

    Refactored code

--- Server.java
+++ Server.java
@@ -3,32 +3,44 @@ import java.net.*;
 
 public class Server {
 	public static void main(String[] args) throws IOException {
-
 		int PORT_NUMB = 4400;
+		DatagramSocket socket;
+		if ((socket = setupUDP(PORT_NUMB)) == null) {
+			return;
+		}
 
-		DatagramSocket sock_list = new DatagramSocket(PORT_NUMB);
-		System.out.println("Started server on " + Integer.toString(PORT_NUMB));
+		
 
+	}
+
+	private static DatagramSocket setupUDP(int port_number) {
+		DatagramSocket socket;
+		try {
+			socket = new DatagramSocket(port_number);
+		}
+		catch (SocketException err) {
+			System.err.println("Failed to create UDP socket!\n" + err.getMessage());
+			return null;
+		}
+
+		return socket;
+	}
+
+	private static void recvMsg(DatagramSocket socket) {
 		byte[] buf = new byte[256];
-		DatagramPacket dp_recived = new DatagramPacket(buf, buf.length);
+		DatagramPacket packet = new DatagramPacket(buf, buf.length);
 
 		try {
-			while (true) {
-				sock_list.receive(dp_recived);
-				System.out.println("Received");
-				try {
-					String str_recive = new String(dp_recived.getData()).trim();
-					System.out.println("Hello got this: "+ str_recive);
-				} finally {
-					System.out.println("Closing the server");
-				}
+			while(true) {
+				socket.receive(packet);
+				String str_recv = new String(packet.getData()).trim();
+				System.out.println("Got this: '" + str_recv + "'");
 			}
 		}
-		finally {
-			System.out.println("Server closed because errors");
-			sock_list.close();
+		catch (IOException err) {
+			System.err.println("Failed to receive message!\n " + err.getMessage());
+			return;
 		}
-
 	}
 
 }
