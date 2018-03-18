07ee5797ebd869fbcec5d9b0e370e055cb98f7e0
Almeida-Oco <intjalmeida@gmail.com>
2018-03-18 00:25:45 +0000

    State handler should be done

--- cli/User_IO.java
+++ cli/User_IO.java
@@ -7,67 +7,76 @@ public class User_IO {
 
   public static void serverUsage() {
     final String prot_name = "  version          ",
-                 prot_desc = "Protocol version to use ( [0-9].[0-9] )\n",
-                 id_name   = "  server id        ",
-                 id_desc   = "ID to assign to the server ( [0-9]+ )\n",
-                 ap_name   = "  access point     ",
-                 ap_desc   = "Service access point to use ( .* ) \n",
-                 mc_name   = "  MC               ",
-                 mc_desc   = "Name of the multicast control channel to use ( <ip>?:?<port> )\n",
-                 mdr_name  = "  MDR              ",
-                 mdr_desc  = "Name of the multicast data recovery channel to use ( <ip>?:?<port> )\n",
-                 mdb_name  = "  MDB              ",
-                 mdb_desc  = "Name of the multicast data channel to use ( <ip>?:?<port> )\n",
-                 ip_name   = "  ip        ",
-                 ip_desc   = "Standard IPv4 address (If missing 127.0.0.1 will be used)\n",
-                 port_name = "  port      ",
-                 port_desc = "Port number to use for the service";
+        prot_desc          = "Protocol version to use ( [0-9].[0-9] )\n",
+        id_name            = "  server id        ",
+        id_desc            = "ID to assign to the server ( [0-9]+ )\n",
+        ap_name            = "  access point     ",
+        ap_desc            = "Service access point to use ( .* ) \n",
+        mc_name            = "  MC               ",
+        mc_desc            = "Name of the multicast control channel to use ( <ip>?:?<port> )\n",
+        mdr_name           = "  MDR              ",
+        mdr_desc           = "Name of the multicast data recovery channel to use ( <ip>?:?<port> )\n",
+        mdb_name           = "  MDB              ",
+        mdb_desc           = "Name of the multicast data channel to use ( <ip>?:?<port> )\n",
+        ip_name            = "  ip        ",
+        ip_desc            = "Standard IPv4 address (If missing 127.0.0.1 will be used)\n",
+        port_name          = "  port      ",
+        port_desc          = "Port number to use for the service";
 
 
     System.err.print("Usage:\n  java Server <version> <server id> <access point> <MC> <MDR> <MDB>\n\n");
     System.err.print("Arguments:\n" +
-                     BOLD + prot_name + PLAIN + prot_desc +
-                     BOLD + id_name + PLAIN + id_desc +
-                     BOLD + ap_name + PLAIN + ap_desc +
-                     BOLD + mc_name + PLAIN + mc_desc +
-                     BOLD + mdr_name + PLAIN + mdr_desc +
-                     BOLD + mdb_name + PLAIN + mdb_desc + "\n\n");
+        BOLD + prot_name + PLAIN + prot_desc +
+        BOLD + id_name + PLAIN + id_desc +
+        BOLD + ap_name + PLAIN + ap_desc +
+        BOLD + mc_name + PLAIN + mc_desc +
+        BOLD + mdr_name + PLAIN + mdr_desc +
+        BOLD + mdb_name + PLAIN + mdb_desc + "\n\n");
     System.err.print("Options:\n" +
-                     BOLD + ip_name + PLAIN + ip_desc +
-                     BOLD + port_name + PLAIN + port_desc + "\n\n");
+        BOLD + ip_name + PLAIN + ip_desc +
+        BOLD + port_name + PLAIN + port_desc + "\n\n");
   }
 
   public static void clientUsage() {
-    final String ap_name      = "  peer_ap        ",
-                 ap_desc      = "Client access point to use ( .* )\n",
-                 prot_name    = "  sub protocol   ",
-                 prot_desc    = "Protocol to initiate. ( <protocol> )\n",
-                 op1_name     = "  operand1       ",
-                 op1_desc     = "First operand of the protocol, optional depending on protocol (see below)\n",
-                 op2_name     = "  operand2       ",
-                 op2_desc     = "Second operand of the protocol, optional depending on protocol (see below)\n",
-                 backup_name  = "  BACKUP    ",
-                 backup_desc  = "Backs up the file specified in " + UNDERLINE + "operand1" + PLAIN + " with the replication degree in " + UNDERLINE + "operand2\n" + PLAIN,
-                 restore_name = "  RESTORE   ",
-                 restore_desc = "Restores the file specified in " + UNDERLINE + "operand1" + PLAIN + " from the network\n",
-                 delete_name  = "  DELETE    ",
-                 delete_desc  = "Deletes the file specified in " + UNDERLINE + "operand1" + PLAIN + " from the network\n",
-                 reclaim_name = "  RECLAIM   ",
-                 reclaim_desc = "Reclaims the memory space specified in " + UNDERLINE + "operand1" + PLAIN + " (memory in KB)\n",
-                 state_name   = "  STATE     ",
-                 state_desc   = "Retrives the current status of the peer (no operand needed)\n";
+    final String ap_name = "  peer_ap        ",
+        ap_desc          = "Client access point to use ( .* )\n",
+        prot_name        = "  sub protocol   ",
+        prot_desc        = "Protocol to initiate. ( <protocol> )\n",
+        op1_name         = "  operand1       ",
+        op1_desc         = "First operand of the protocol, optional depending on protocol (see below)\n",
+        op2_name         = "  operand2       ",
+        op2_desc         = "Second operand of the protocol, optional depending on protocol (see below)\n",
+        backup_name      = "  BACKUP    ",
+        backup_desc      = "Backs up the file specified in " + UNDERLINE + "operand1" + PLAIN + " with the replication degree in " + UNDERLINE + "operand2\n" + PLAIN,
+        restore_name     = "  RESTORE   ",
+        restore_desc     = "Restores the file specified in " + UNDERLINE + "operand1" + PLAIN + " from the network\n",
+        delete_name      = "  DELETE    ",
+        delete_desc      = "Deletes the file specified in " + UNDERLINE + "operand1" + PLAIN + " from the network\n",
+        reclaim_name     = "  RECLAIM   ",
+        reclaim_desc     = "Reclaims the memory space specified in " + UNDERLINE + "operand1" + PLAIN + " (memory in KB)\n",
+        state_name       = "  STATE     ",
+        state_desc       = "Retrives the current status of the peer (no operand needed)\n";
 
     System.out.print("Usage:\n  java Client <peer_ap> <sub protocol> <operand1> <operand2>\n\n");
     System.err.print("Arguments:\n" +
-                     BOLD + ap_name + PLAIN + ap_desc +
-                     BOLD + prot_name + PLAIN + prot_desc +
-                     BOLD + op1_name + PLAIN + op1_desc +
-                     BOLD + op2_name + PLAIN + op2_desc + "\n\n");
+        BOLD + ap_name + PLAIN + ap_desc +
+        BOLD + prot_name + PLAIN + prot_desc +
+        BOLD + op1_name + PLAIN + op1_desc +
+        BOLD + op2_name + PLAIN + op2_desc + "\n\n");
     System.err.print("Protocols:\n" +
-                     BOLD + backup_name + PLAIN + backup_desc +
-                     BOLD + restore_name + PLAIN + restore_desc +
-                     BOLD + delete_name + PLAIN + delete_desc +
-                     BOLD + reclaim_name + PLAIN + reclaim_desc +
-                     BOLD + state_name + PLAIN + state_desc + "\n\n");
+        BOLD + backup_name + PLAIN + backup_desc +
+        BOLD + restore_name + PLAIN + restore_desc +
+        BOLD + delete_name + PLAIN + delete_desc +
+        BOLD + reclaim_name + PLAIN + reclaim_desc +
+        BOLD + state_name + PLAIN + state_desc + "\n\n");
+  }
+
+  public static void printState(Files files) {
+    files.getFiles().forEach((file_name, info)->{
+      System.out.println(BOLD + file_name + PLAIN);
+      info.forEach((number, size)->{
+        System.out.println(BOLD + "  #" + number + PLAIN + " - (" + size + "KB)");
+      });
+    });
   }
 }
--- controller/client/StateHandler.java
+++ controller/client/StateHandler.java
@@ -1,10 +1,14 @@
 package controller.client;
 
+import cli.User_IO;
+import cli.Files;
+import files.*;
 import network.PacketInfo;
 import controller.Handler;
 import controller.Pair;
 
-
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Vector;
 import java.rmi.Remote;
 
 class StateHandler extends Handler implements Remote {
@@ -24,6 +28,18 @@ class StateHandler extends Handler implements Remote {
 
   @Override
   public void run() {
+    ConcurrentHashMap<String, FileInfo> file_table = File_IO.getTable();
+    Files file_info = new Files();
+
+    file_table.forEach(1, (file_name, info)->{
+      file_info.addNewFile(file_name);
+      Vector<FileChunk> chunks = info.getChunks();
+      chunks.forEach((chunk)->{
+        file_info.addChunk(file_name, chunk.getChunkN(), chunk.getSize());
+      });
+    });
+
+    User_IO.printState(file_info);
     System.out.println("GOT STATE MSG!");
   }
 }
