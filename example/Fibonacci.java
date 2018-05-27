b94275894a8364f5ab8e4399c105904089ec9233
GonVas <goncalo.moreno97@gmail.com>
2018-05-27 22:51:24 +0100

    Added compile multiple

---
+++ example/Fibonacci.java
@@ -0,0 +1,16 @@
+public class Fibonacci {
+
+public static void main (String[] args) {
+    int f = 0;
+    int g = 1;
+
+    for (int i = 1; i <= 100; i++) {
+        System.out.print(f + " ");
+        f = f + g;
+        g = f - g;
+    } 
+
+    System.out.println();
+}
+
+}
